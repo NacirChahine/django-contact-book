f49a837dc0d7a4cf498abf8356cf6cecb7bc5492
Nacir <nacir.chahine@ismena.com>
2024-02-09 21:16:44 +0200

    first commit

---
+++ contacts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
